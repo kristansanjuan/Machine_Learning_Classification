8a0e56b344d0e0b4185c7adbeccf0175fef36dd0
A Gaming <nagatopain069@gmail.com>
2025-04-24 12:41:06 +0800

    remove max_features

--- Machine_Learning/vectorize.py
+++ Machine_Learning/vectorize.py
@@ -4,5 +4,4 @@ def vectorize_texts(texts):
     vectorizer = TfidfVectorizer(max_features=5000)
     X = vectorizer.fit_transform(texts)
     print(f"Shape of TF-IDF matrix: {X.shape}")
-    print("TF-IDF vectorizer max features:", vectorizer.max_features)
     return X, vectorizer
